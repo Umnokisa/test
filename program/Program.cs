da90dd7ef7333e5149516a83575b0040b3b18f4a
Umnokisa <kudahtalka@gmail.com>
2023-03-14 19:34:52 +0300

    Create program

---
+++ program/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] n;
+Console.WriteLine("Введите массив строк через пробел");
+string? line = Console.ReadLine();
+n = line.Split(' ');
+var result = new string[n .Length];
+var size = 0;
+ foreach (var value in n )
+{
+     if (value.Length <= 3)
+    {
+        result[size] = value;
+        size++;
+    }
+}
+Console.WriteLine("Только эти с не более чем тремя символами:");
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, size));
